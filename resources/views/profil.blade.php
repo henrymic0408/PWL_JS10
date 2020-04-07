4cf90d4551c22df826e3ddf86d06e1eaef9723c2
henrymic0408 <henrymic0408@gmail.com>
2020-04-07 11:15:31 +0700

    First commit

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Profil Saya</title>
+</head>
+
+<body>
+    <h1>Profil Saya</h1>
+    <p>Perkenalkan Nama saya Henry Michael, saya seorang Mahasiswa Polinema Kelas TI-2A No. Absen 12
+        .</p>
+</body>
+
+</html>
